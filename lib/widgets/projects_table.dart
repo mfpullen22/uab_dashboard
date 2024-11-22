2b803f5f66edcf3ccf8982c06806509cc37c8e9e
Matthew Pullen <matthew.pullen@gmail.com>
2024-11-22 15:42:05 -0600

    Added bottom border to data rows in table

--- lib/widgets/projects_table.dart
+++ lib/widgets/projects_table.dart
@@ -10,170 +10,169 @@ class ProjectsTable extends StatelessWidget {
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
-      child: DataTable(
-        columns: const [
-          DataColumn(label: Text('Title/PI')),
-          DataColumn(label: Text('Presented')),
-          DataColumn(label: Text('Approved')),
-          DataColumn(label: Text('IRB Number')),
-          DataColumn(label: Text('Activated')),
-          DataColumn(label: Text('Enrolled')),
-          DataColumn(label: Text('Data Collection')),
-          DataColumn(label: Text('Close Out')),
-          DataColumn(label: Text('Comments')),
-        ],
-        rows: projects.map(
+      child: Column(
+        children: projects.map(
           (project) {
-            return DataRow(
-              cells: [
-                DataCell(
-                  SizedBox(
-                    width: 200, // Fixed width for the column
-                    child: Wrap(
-                      spacing: 0, // No spacing between wrapped elements
-                      runSpacing: 4, // Add spacing between lines
-                      children: [
-                        Text(
-                          project.title,
-                          style: const TextStyle(fontWeight: FontWeight.bold),
-                        ),
-                        Row(
-                          children: [
-                            const Text(
-                              "PI: ",
-                              style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  fontWeight: FontWeight.bold),
-                            ),
-                            Text(project.pi),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
+            return Container(
+              decoration: const BoxDecoration(
+                border: Border(
+                  bottom:
+                      BorderSide(color: Colors.grey, width: 2), // Divider line
                 ),
-                DataCell(Text(project.presentedDate)),
-                DataCell(SizedBox(
-                  width: 150,
-                  child: Wrap(
-                    spacing: 0,
-                    runSpacing: 4,
-                    children: [
-                      Row(
-                        children: [
-                          const Text(
-                            "SC: ",
-                            style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontWeight: FontWeight.bold),
+              ),
+              child: DataTable(
+                dataRowMinHeight: 50,
+                dataRowMaxHeight: double.infinity,
+                columns: const [
+                  DataColumn(label: Text('Title/PI')),
+                  DataColumn(label: Text('Presented')),
+                  DataColumn(label: Text('Approved')),
+                  DataColumn(label: Text('IRB Number')),
+                  DataColumn(label: Text('Activated')),
+                  DataColumn(label: Text('Enrolled')),
+                  DataColumn(label: Text('Data Collection')),
+                  DataColumn(label: Text('Close Out')),
+                  DataColumn(label: Text('Comments')),
+                ],
+                rows: [
+                  DataRow(
+                    cells: [
+                      DataCell(
+                        SizedBox(
+                          width: 200,
+                          child: Wrap(
+                            spacing: 0,
+                            runSpacing: 4,
+                            children: [
+                              Text(
+                                project.title,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "PI: ",
+                                    style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(project.pi),
+                                ],
+                              ),
+                            ],
                           ),
-                          Text(project.approvalSCDate),
-                        ],
+                        ),
                       ),
-                      Row(
-                        children: [
-                          const Text(
-                            "CDC: ",
-                            style: TextStyle(
-                                decoration: TextDecoration.underline,
-                                fontWeight: FontWeight.bold),
+                      DataCell(Text(project.presentedDate)),
+                      DataCell(
+                        SizedBox(
+                          width: 150,
+                          child: Wrap(
+                            spacing: 0,
+                            runSpacing: 4,
+                            children: [
+                              Row(
+                                children: [
+                                  const Text(
+                                    "SC: ",
+                                    style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(project.approvalSCDate),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Text(
+                                    "CDC: ",
+                                    style: TextStyle(
+                                        decoration: TextDecoration.underline,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                  Text(project.approvalCDCDate),
+                                ],
+                              ),
+                            ],
                           ),
-                          Text(project.approvalCDCDate),
-                        ],
+                        ),
                       ),
-                      Row(
-                        children: [
-                          const Text("IRB: ",
-                              style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  fontWeight: FontWeight.bold)),
-                          Text(project.approvalIRBDate),
-                        ],
+                      DataCell(
+                        Text(project.irbNumber.isNotEmpty
+                            ? project.irbNumber
+                            : 'Pending'),
                       ),
-                      Row(
-                        children: [
-                          const Text("Contract: ",
-                              style: TextStyle(
-                                  decoration: TextDecoration.underline,
-                                  fontWeight: FontWeight.bold)),
-                          Text(project.approvalContractDate.isEmpty
-                              ? 'Pending'
-                              : project.approvalContractDate),
-                        ],
+                      DataCell(
+                        Text(project.activatedDate.isNotEmpty
+                            ? project.activatedDate
+                            : 'Pending'),
+                      ),
+                      DataCell(
+                        SizedBox(
+                          width: 150,
+                          child: Wrap(
+                            spacing: 0,
+                            runSpacing: 4,
+                            children: [
+                              Row(
+                                children: [
+                                  const Text("Screened: "),
+                                  Text(project.enrollment["screened"].isNotEmpty
+                                      ? project.enrollment["screened"]
+                                      : '0'),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  const Text("Enrolled: "),
+                                  Text(project.enrollment["enrolled"].isNotEmpty
+                                      ? project.enrollment["enrolled"]
+                                      : '0'),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      DataCell(
+                        Text(project.dataCollection.isNotEmpty
+                            ? project.dataCollection
+                            : 'Pending'),
+                      ),
+                      DataCell(
+                        Text(project.closeOut.isNotEmpty
+                            ? project.closeOut
+                            : 'Pending'),
+                      ),
+                      DataCell(
+                        SizedBox(
+                          width: 150,
+                          child: Wrap(
+                            children: project.comments.entries.map(
+                              (entry) {
+                                return Row(
+                                  children: [
+                                    Text(
+                                      entry.key,
+                                      style: const TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(width: 8),
+                                    Expanded(
+                                      child: Text(entry.value),
+                                    ),
+                                  ],
+                                );
+                              },
+                            ).toList(),
+                          ),
+                        ),
                       ),
                     ],
                   ),
-                )),
-                DataCell(
-                  Text(project.irbNumber.isNotEmpty
-                      ? project.irbNumber
-                      : 'Pending'),
-                ),
-                DataCell(
-                  Text(project.activatedDate.isNotEmpty
-                      ? project.activatedDate
-                      : 'Pending'),
-                ),
-                DataCell(
-                  SizedBox(
-                    width: 150,
-                    child: Wrap(
-                      spacing: 0,
-                      runSpacing: 4,
-                      children: [
-                        Row(
-                          children: [
-                            const Text("Screened: "),
-                            Text(project.enrollment["screened"].isNotEmpty
-                                ? project.enrollment["screened"]
-                                : '0'),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            const Text("Enrolled: "),
-                            Text(project.enrollment["enrolled"].isNotEmpty
-                                ? project.enrollment["enrolled"]
-                                : '0'),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                DataCell(
-                  Text(project.dataCollection.isNotEmpty
-                      ? project.dataCollection
-                      : 'Pending'),
-                ),
-                DataCell(
-                  Text(project.closeOut.isNotEmpty
-                      ? project.closeOut
-                      : 'Pending'),
-                ),
-                DataCell(
-                  SizedBox(
-                    width: 150,
-                    child: Wrap(
-                      children: project.comments.entries.map((entry) {
-                        return Row(
-                          children: [
-                            Text(
-                              entry.key,
-                              style:
-                                  const TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                            const SizedBox(width: 8),
-                            Expanded(
-                              child: Text(entry.value),
-                            ),
-                          ],
-                        );
-                      }).toList(),
-                    ),
-                  ),
-                ),
-              ],
+                ],
+              ),
             );
           },
         ).toList(),
